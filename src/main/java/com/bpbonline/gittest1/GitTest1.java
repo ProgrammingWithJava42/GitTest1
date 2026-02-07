aaeb03c39441152c891fa354caa5bf0b80ea431f
Ken Fogel <programmingwithjava42@users.noreply.github.com>
2026-02-07 14:52:02 -0500

    The first commit of GitTest1

---
+++ src/main/java/com/bpbonline/gittest1/GitTest1.java
@@ -0,0 +1,13 @@
+package com.bpbonline.gittest1;
+
+/**
+ * Sample program for using Git
+ *
+ * @author Ken Fogel
+ */
+public class GitTest1 {
+
+    void main() {
+        IO.println("Hello Maven and Git!");
+    }
+}
